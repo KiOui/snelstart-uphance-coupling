11ab8ffce74213efb6b496da650e2d39d452deeb
Lars van Rhijn <larsvanrhijn@gmail.com>
2021-10-27 09:21:01 +0200

    Start of project

---
+++ snelstart-uphance-coupling/snelstart-uphance-coupling.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Plugin Name: Snelstart Uphance Coupling
+ * Description: A utility to synchronize Snelstart and Uphance using API endpoints
+ * Plugin URI: https://github.com/KiOui/snelstart-uphance-coupling
+ * Version: 0.0.1
+ * Author: Lars van Rhijn
+ * Author URI: https://larsvanrhijn.nl/
+ * Text Domain: snelstart-uphance-coupling
+ * Domain Path: /languages/
+ *
+ * @package snelstart-uphance-coupling
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! defined( 'SUC_PLUGIN_FILE' ) ) {
+	define( 'SUC_PLUGIN_FILE', __FILE__ );
+}
+if ( ! defined( 'SUC_PLUGIN_URI' ) ) {
+	define( 'SUC_PLUGIN_URI', plugin_dir_url( __FILE__ ) );
+}
+
+include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+
+// include_once dirname( __FILE__ ) . '/includes/class-aicore.php';
+
+// $GLOBALS['AICore'] = AICore::instance();
